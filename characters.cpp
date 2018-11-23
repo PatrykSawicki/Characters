40a9bb5584d5f23c6c6f1c5598689f91bf6705d8
Patryk_S <44925137+PatrykSawicki@users.noreply.github.com>
2018-11-23 22:04:07 +0100

    Update characters.cpp

--- characters.cpp
+++ characters.cpp
@@ -5,54 +5,58 @@ string password;
 
 int main()
 {
-  cout << "\nPlease enter the password: " ; //1. Wprowadzenie danych
+  cout << "\nPlease enter the password: " ; // data entry
   cin  >> password;
   cout << "\nYour password:  "<< password <<"  is "
-       << password.size()<<" characters long\n"<< endl; //2. uzyskanie ilości elementów tablicy string
+       << password.size()<<" characters long\n" << endl; // getting the string array size
 //---------------------------------------------------------------------------------------------------------------------------------------------------
-  int dimension = password.size(); //rozmiar tablicy zależny od długości wprowadzonego stringa
+  int dimension = password.size(); // array size depending on the length of the string entered
   int element = 0;
-  int tab_element[dimension];      // zliczenie wystąpień poszczególnych elementów
+  int tab_element[dimension];      // count of occurences of element
   string array[dimension];
 
-  int largest = 0; //zmienna pomocnicza: największa liczba wystąpień
-  string big ;   //zmienna pomocnicza: znak, który miał największą liczbę wystapień
+  int largest = 0;  // auxiliary variable: the larges numbers of occurences
+  string big ;      // auxiliary variable: character, which had the highest number of occurences
 
   string tab_new[dimension];
   bool repeat = false;
   int counter = 0;
 
   for(int k = 0; k < dimension; k++){
-    
+
         for(int i = 0; i < dimension; i++)
         {
-            array[i] = password[i];  //przepisanie tablicy
+            array[i] = password[i];     // rewriting the array
             if (array[k] == array[i])
-               {element++ ;} //zwiekszenie ilości danego elementu w przebiegu pętli
+               {element++ ;}            // element increment
+
         }
-        tab_element[k] = element;  //skopiowanie uzyskanej ilości danego 'k' elementu tablicy
+        tab_element[k] = element;       // coppying the calculated amount of the given value k-element of the array
 
         if (tab_element[k] > largest)
            {largest = tab_element[k];
-           big = array[k];} //znalezienie najwiekszj ilości elementów
+           big = array[k];}             // finding the most items
 
-        element = 0; //wyzerowanie ilości elementów dla kolejnego 'k' przebiegu tablicy
+        element = 0;                    // reset the number of elements fot the k-table run
+
 //----------------------------------------------------------------------------------------------------------------------------------------------//
         for(int z = 0; z < dimension; z++)
         {
             if (tab_new[z] == array[k]){repeat = true;}
-            //jeśli element w nowo utworzonej tablicy się powtarza oznacz go w celu pominięcia podczas wydruku i wyjdź z pętli
+            // if the element in the newly created array is repeated, mark it to skip the printout and exit the loop
         }
         if(!repeat)
-          //Jeśli element  nowej tablicy nie powtarza się to dodaj ka-ty element do nowej tablicy, wydrukuj go i zwiększ licznik  o 1
+            // if the element of the new array does not repeat, add each element to the new table, print it and increase the counter by 1
           {tab_new[counter] = array[k];
           cout<< "Element: "<< tab_new[counter]<<" ocurred " << tab_element[k]<<" times."<<endl;
           counter ++;}
+
         else {repeat = false;}
   }
+
   cout << "\nThe largest \'"<< big <<"\' occured: "<< largest <<" times"<<endl;
   cout << endl;
   cin  >> password;
-  
+
 return 0;
 }
